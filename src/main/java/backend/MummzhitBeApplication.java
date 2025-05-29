8f67889137f9330f3465c82ba57b1e2e127e6464
kyoohyunlee <khyhlee@naver.com>
2025-05-29 17:05:24 +0900

    project 초기 세팅

---
+++ src/main/java/backend/MummzhitBeApplication.java
@@ -0,0 +1,13 @@
+package backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MummzhitBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MummzhitBeApplication.class, args);
+    }
+
+}
