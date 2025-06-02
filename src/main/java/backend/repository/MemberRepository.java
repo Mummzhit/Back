483931a4b82a2542193db57af32f448e8e39e9ba
kyoohyunlee <khyhlee@naver.com>
2025-06-03 03:08:08 +0900

    feat: 로그인/회원가입 repository

--- src/main/java/backend/repository/MemberRepository.java
+++ src/main/java/backend/repository/MemberRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByLoginId(String loginId);
-    boolean existsByLoginId(String loginId);
+    Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
