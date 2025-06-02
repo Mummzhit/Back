d80d94b113f5c7d65b0cce1fb98a0e1c2fda6e3c
kyoohyunlee <khyhlee@naver.com>
2025-06-03 03:07:13 +0900

    feat: 로그인/회원가입 Controller

---
+++ src/main/java/backend/controller/AuthController.java
@@ -0,0 +1,35 @@
+package backend.controller;
+
+import backend.dto.MemberRequestDto;
+import backend.dto.MemberResponseDto;
+import backend.dto.TokenDto;
+import backend.dto.TokenRequestDto;
+import backend.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<MemberResponseDto> signup(@RequestBody MemberRequestDto memberRequestDto) {
+        return ResponseEntity.ok(authService.signup(memberRequestDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto memberRequestDto) {
+        return ResponseEntity.ok(authService.login(memberRequestDto));
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
+        return ResponseEntity.ok(authService.reissue(tokenRequestDto));
+    }
+}
