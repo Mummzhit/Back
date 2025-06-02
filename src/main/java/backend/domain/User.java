f29092ed3440c1d1f0cbf268eec5fa1fccd868f4
kyoohyunlee <khyhlee@naver.com>
2025-06-02 23:12:51 +0900

    feat: User테이블 컬럼 카멜 케이스로 변경

--- src/main/java/backend/domain/User.java
+++ src/main/java/backend/domain/User.java
@@ -8,23 +8,26 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Table(name = "user")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
-    private String login_id;
+    @Column(name = "loginId", unique = true, nullable = false)
+    private String loginId;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String nickname;
 
     //중복 관련해서 원하면 추가하자
+    @Column(nullable = false)
     private String password;
 
     //SpringSecurity 사용할거면 권한 이런 것도 만들 수 있는데, 지금은 하지말자
 
     @CreationTimestamp
+    @Column(name = "createdAt", updatable = false, nullable = false)
     private LocalDateTime createdAt;
 }
